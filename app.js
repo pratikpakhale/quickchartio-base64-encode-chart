def731d6bba76280dfaa513e37d3acb06c3a695b
pratikpakhale <pratikpakhale20@gmail.com>
2022-12-02 11:48:21 +0530

    added post req for obj

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let express = require('express')
 const axios = require('axios')
 let app = express()
 
+app.use(express.json())
+
 app.get('/generate', async function (req, res) {
   let query = req.query || ''
   let url = ' https://quickchart.io/chart'
@@ -14,6 +16,33 @@ app.get('/generate', async function (req, res) {
       c: query.c || '',
       bkg: query.bkg || 'white',
     },
+  }
+  axios(config)
+    .then(function (response) {
+      let base64 = Buffer.from(response.data, 'binary').toString('base64')
+      return res.json({
+        base64,
+      })
+    })
+    .catch(function (error) {
+      return res.json({
+        error,
+      })
+    })
+})
+
+app.post('/generate', async function (req, res) {
+  let query = req.body || ''
+  let url =
+    ' https://quickchart.io/chart?' +
+    new URLSearchParams({
+      bkg: query.bkg || 'white',
+      c: JSON.stringify(query.c) || '',
+    }).toString()
+  let config = {
+    method: 'get',
+    responseType: 'arraybuffer',
+    url: url,
   }
   axios(config)
     .then(function (response) {
